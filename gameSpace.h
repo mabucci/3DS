78fe5cd87e87593e4cb2b67077fe941ea6d92224
mabucci <69395031+mabucci@users.noreply.github.com>
2026-07-11 12:26:14 -0400

    Update gameSpace.h

--- gameSpace.h
+++ gameSpace.h
@@ -19,4 +19,7 @@ public:
   void buildGameSpace();
   void upDateGoalsPositioinAndDimensions();
   void testIfAnyIconBumped();
+  void testGoalContentionWinner(); // Test for the winner of GOAL contention   Set winner as stage two possession, and eject loser   
+  int randomNumberGenerator(int, int);  // A general Random Number Generator method   takes two arguments that make the [inclusive range] 
+  void testCorrectKickOutPosition(std::string);  // Test and or correct the new kicked out to position, if kicked out position is out of game space then clip that position to keep the ICON in bounds  
 };
