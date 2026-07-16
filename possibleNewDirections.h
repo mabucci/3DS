316e8742fd7eae64774ab4e7a6d6dc63cea8f7b2
mabucci <69395031+mabucci@users.noreply.github.com>
2026-07-15 21:23:14 -0400

    Update possibleNewDirections.h

--- possibleNewDirections.h
+++ possibleNewDirections.h
@@ -1,5 +1,10 @@
-static const std::string greetingString = "Welcome to "Top Trumps 3D", this is a test bed for game logic.\n\
-One human plays against the program.  
-At game start a deck of standard playing cards is shuffled and dealt out to PLAYER and GAME, each gets twenty-six cards, no "Joker".
-Objective is to occupy the GOAL;  the GOAL is a three-dimensional space that is located somewhere in the GAME-SPACE;  the GAME-SPACE is the playing area of the game.
+#pragma once
+
+static const std::string greetingString = "Welcome to \"Top Trumps 3D\", this is a test bed for game logic.\n\
+Human(s) play against the program.\n\  
+At game start a deck of standard playing cards is shuffled and dealt out to PLAYER and GAME, each gets twenty-six cards, no \"Joker\".\n\
+The objective of the game is to occupy the GOAL;  the GOAL is a three-dimensional space that is located somewhere in the GAME-SPACE;  the GAME-SPACE is the playing area of the game.\n\
+It is also a three-dimensional space with (X axis) length of 130 game-space-units, (Y axis) depth of 30 game-space-units, and (Z axis) height of 100 game-space-units.\n\
+The GOAL is displayed on the GAME-SPACE map as a rectangle, the GAME-SPACE map is a two-dimensional display, it shows length (X axis) and width (Y axis) , but no height (Z axis).\n\
+
 ";
