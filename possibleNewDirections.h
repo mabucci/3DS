8f63714bbecfd67feddad1846f77f4ed3be6998e
mabucci <69395031+mabucci@users.noreply.github.com>
2026-07-15 21:04:33 -0400

    Update possibleNewDirections.h

--- possibleNewDirections.h
+++ possibleNewDirections.h
@@ -1,4 +1,5 @@
-    Welcome to "Top Trumps 3D", this is a test bed for game logic.  
+static const std::string greetingString = "Welcome to "Top Trumps 3D", this is a test bed for game logic.\n\
 One human plays against the program.  
 At game start a deck of standard playing cards is shuffled and dealt out to PLAYER and GAME, each gets twenty-six cards, no "Joker".
 Objective is to occupy the GOAL;  the GOAL is a three-dimensional space that is located somewhere in the GAME-SPACE;  the GAME-SPACE is the playing area of the game.
+";
