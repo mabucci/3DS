4e2038165418d1c35a9168f1bae078280cc4e0f3
mabucci <69395031+mabucci@users.noreply.github.com>
2026-07-15 20:57:38 -0400

    Update possibleNewDirections.h

--- possibleNewDirections.h
+++ possibleNewDirections.h
@@ -1,1 +1,4 @@
-
+    Welcome to "Top Trumps 3D", this is a test bed for game logic.  
+One human plays against the program.  
+At game start a deck of standard playing cards is shuffled and dealt out to PLAYER and GAME, each gets twenty-six cards, no "Joker".
+Objective is to occupy the GOAL;  the GOAL is a three-dimensional space that is located somewhere in the GAME-SPACE;  the GAME-SPACE is the playing area of the game.
