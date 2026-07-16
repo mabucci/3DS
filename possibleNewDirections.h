6b46db519961428c4b3953ef70eba322b8b9be1f
mabucci <69395031+mabucci@users.noreply.github.com>
2026-07-15 22:34:45 -0400

    Update possibleNewDirections.h

--- possibleNewDirections.h
+++ possibleNewDirections.h
@@ -9,5 +9,8 @@ The GOAL is displayed on the GAME-SPACE map as a rectangle, the GAME-SPACE map i
 Both PLAYER and GAME's position on the GAME-SPACE map are shown as the capital letter P, and G respectively.   If both occupy the same (x, y) coordinate the capital letter B will be displayed.\n\    
 The Z axis location or height, of the PLAYER, GAME, and GOAL, along with their X, and Y axis location are displayed on the screen along side the GAME-SPACE map.\n\
 To occupy the GOAL, PLAYER or GAME must first move into the GOAL.  This is done by simply moving into the GOAL's area.   The PLAYER has at the start of the game 26 cards;\n\
-each card has a unique movement value.     
+each card has a unique movement value.  The movement value is the sum of the face value of the card (Ace==1, 2==2... 10==10, Jack==11, King==13), plus the suit value of the \n\
+card (Spade==1, Club==2, Diamond==3, Heart==4.)    qed: the FOUR of CLUBS has a value of six movement points.    Each movement point can be used to move from one game-space-location\n\
+to another.   The points can be used all in moving on one axis or spread out over all three axis.   qed:  PLAYER is as X==i, Y==j, Z==k, wants to move to X==i+3, Y==j-4, Z==k+2, \n\
+the sum of the movement points needed is 3+4+2==9==EIGHT of SPADES==SEVEN of CLUBS==SIX of DIMONDS==FIVE of HEARTS.    If the PLAYER has any of those four cards the PLAYER can make that move.   
 ";
