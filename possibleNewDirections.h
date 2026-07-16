746f687ffc5a1df5bc7938528fc320cd78d78489
mabucci <69395031+mabucci@users.noreply.github.com>
2026-07-15 21:29:46 -0400

    Update possibleNewDirections.h

--- possibleNewDirections.h
+++ possibleNewDirections.h
@@ -6,5 +6,6 @@ At game start a deck of standard playing cards is shuffled and dealt out to PLAY
 The objective of the game is to occupy the GOAL;  the GOAL is a three-dimensional space that is located somewhere in the GAME-SPACE;  the GAME-SPACE is the playing area of the game.\n\
 It is also a three-dimensional space with (X axis) length of 130 game-space-units, (Y axis) depth of 30 game-space-units, and (Z axis) height of 100 game-space-units.\n\
 The GOAL is displayed on the GAME-SPACE map as a rectangle, the GAME-SPACE map is a two-dimensional display, it shows length (X axis) and width (Y axis) , but no height (Z axis).\n\
-
+Both PLAYER and GAME's position on the GAME-SPACE map is shown as the capital letter 'P', and 'G' respectively.     If both occupy the same (x, y) coordinate the capital letter\n\
+'B' will be displayed.     
 ";
